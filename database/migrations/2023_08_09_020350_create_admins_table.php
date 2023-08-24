bb56504521574d229f98cc0427c49dc395dcd40b
MohaDevel <mohalhakami2019@gmail.com>
2023-08-24 04:14:28 +0300

    the table is Admin in the datebase

---
+++ database/migrations/2023_08_09_020350_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email',100);
+            $table->string('username',100);
+            $table->string('password',225);
+            $table->integer('added_by');
+            $table->integer('com_code');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
